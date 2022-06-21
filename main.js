582e456e6f7d70a531af05f18f84ba65e44f4364
rishitkundu1 <85060575+rishitkundu1@users.noreply.github.com>
2022-06-21 18:11:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 function setup(){
       canvas = createCanvas(900,600);
       canvas.center();
+      video = createCapture(VIDEO);
+      video.hide();
+}
+function preload(){
+      song1 = loadSound("music.mp3");
+      song2 = loadSound("BelieverMP3.mp3");
+}
+function draw(){
+      image(video, 0, 0, 900, 600);
 }
